3e7e70d046a4e724a2830db42b93b2614fb893e4
Gurukiran SN <gurukiran.sn@netenrich.com>
2025-05-26 16:29:18 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   build: { target: "esnext" , modulePreload : false , minify : false , cssCodeSplit : false },
   server: {
     port: 4005,
+    cors :false
   },
-  base: "./",
+  base: "/",
 });
