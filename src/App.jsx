68e3302e5a26c0d75d40e8035d0dd0abe2d592ac
Gurukiran SN <gurukiran.sn@netenrich.com>
2025-05-26 18:30:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./App.css";
 import LoginPage from "./pages/login/LoginPage";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <LoginPage />
+      </Auth0Provider>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </StrictMode>
 );
